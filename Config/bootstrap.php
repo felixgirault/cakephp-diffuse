cd52ca13778a07913daf6f871e8106c2d6e72168
Félix Girault <felix.girault@gmail.com>
2014-02-08 18:43:33 +0100

    First version

---
+++ Config/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ *	Diffuse autoload.
+ */
+
+require_once dirname( __FILE__ )
+	. DS . '..'
+	. DS . 'Vendor'
+	. DS . 'autoload.php';
---
+++ View/Helper/DiffuseHelper.php
@@ -0,0 +1,104 @@
+<?php
+
+use Diffuse\Diffuse;
+
+
+
+/**
+ *	A simple proxy to configure and use Diffuse.
+ *
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@package Essence.Model.Behavior
+ *	@license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+class DiffuseHelper extends AppHelper {
+
+	/**
+	 *	Helpers.
+	 *
+	 *	@var array
+	 */
+
+	public $helpers = array( 'Html' );
+
+
+
+	/**
+	 *	Custom services for Diffuse.
+	 *
+	 *	@var array
+	 */
+
+	public $services = array( );
+
+
+
+	/**
+	 *	Diffuse instance.
+	 *
+	 *	@var Diffuse\Diffuse
+	 */
+
+	protected $_Diffuse = null;
+
+
+
+	/**
+	 *	Constructor.
+	 *
+	 *	@param View $View View using the helper.
+	 *	@param array $settings Helper settings.
+	 */
+
+	public function __construct( View $View, array $settings = array( )) {
+
+		parent::__construct( $View, $settings );
+
+		$this->_Diffuse = new Diffuse( $this->services );
+	}
+
+
+
+	/**
+	 *	Provides shortcuts to the url( ) method:
+	 *
+	 *	@code
+	 *	$Diffuse->link( 'facebook', 'Share on Facebook', 'http://example.com/page' );
+	 *	// or
+	 *	$Diffuse->facebook( 'Share on Facebook', 'http://example.com/page' );
+	 *	@endcode
+	 *
+	 *	@param string $service Service name.
+	 *	@param array $arguments Arguments.
+	 *	@return string URL.
+	 */
+
+	public function __call( $service, $arguments = array( )) {
+
+		array_unshift( $arguments, $service );
+
+		return call_user_func_array( array( $this, 'link' ), $arguments );
+	}
+
+
+
+	/**
+	 *	Builds and returns a link tag to share an URL on a social network.
+	 *
+	 *	@param string $service Service name.
+	 *	@param string $text Text inside the link.
+	 *	@param string|array $params URL to share or a set of Diffuse params.
+	 *	@param array $options Options and HTML attributes.
+	 *	@return string Link tag.
+	 */
+
+	public function link( $service, $text, $params, array $options = array( )) {
+
+		return $this->Html->link(
+			$text,
+			$this->_Diffuse->url( $service, $params ),
+			$options
+		);
+	}
+}
